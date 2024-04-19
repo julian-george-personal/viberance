039a1264ff6ef42e1f57b9e086be7ca1a9be696d
Julian George <juliancgeorge@gmail.com>
2024-04-19 17:38:33 -0400

    pre debug

--- src/meshes/LightOrb.tsx
+++ src/meshes/LightOrb.tsx
@@ -1,11 +1,17 @@
 import { MeshPhongMaterial } from "three";
-import { animated } from "@react-spring/three";
+import { animated, useSpring } from "@react-spring/three";
 
 const LightOrb = ({ position, color, radius }: any) => {
+  const animatedColor = useSpring({ color: "blue" });
+
   return (
     <mesh position={position}>
       <sphereGeometry args={[radius, 128, 128]} />
-      <animated.meshPhongMaterial color={color} />
+      <animated.meshLambertMaterial
+        emissive={color}
+        color={color}
+        reflectivity={50}
+      />
     </mesh>
   );
 };

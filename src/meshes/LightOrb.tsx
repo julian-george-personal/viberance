37f4350b0e10dd7640a8c9506a15ef3468cbf2f8
Julian George <juliancgeorge@gmail.com>
2024-04-19 18:02:48 -0400

    e

--- src/meshes/LightOrb.tsx
+++ src/meshes/LightOrb.tsx
@@ -2,6 +2,7 @@ import { MeshPhongMaterial } from "three";
 import { animated, useSpring } from "@react-spring/three";
 
 const LightOrb = ({ position, color, radius }: any) => {
+  console.log(color);
   return (
     <mesh position={position}>
       <sphereGeometry args={[radius, 128, 128]} />
